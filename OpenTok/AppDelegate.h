4e5783ab2f0332edca2b2a07ae68c57a20ae773e
lenhador <belov.alexandr1991@gmail.com>
2013-05-24 19:31:11 +0400

    finished

---
+++ OpenTok/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenTok
+//
+//  Created by Администратор on 5/24/13.
+//  Copyright (c) 2013 Lenhador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
